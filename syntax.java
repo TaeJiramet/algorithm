0a130630c3d244cd478894bf90f8e36b9799b378
TaeJiramet <674259004@webmail.npru.ac.th>
2024-07-30 11:57:05 +0700

    Jiramet

---
+++ syntax.java
@@ -0,0 +1,7 @@
+public class syntax {
+public static void main(String[] args){
+    System.out.println("Hello World"); //syntax error
+    System.out.println(4/2); //runtime eror
+    System.out.println(4/100);
+ }
+}
